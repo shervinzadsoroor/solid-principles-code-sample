ba3921aec1040a0295ac8a3d309ecced0a989aa3
shervin zadsoroor <57758768+shervinzadsoroor@users.noreply.github.com>
2021-05-30 14:45:53 +0430

    Create Animal.java

---
+++ openclose/Animal.java
@@ -0,0 +1,10 @@
+package openclose;
+
+public class Animal {
+    protected void breathing() {
+        System.out.println("the animal is breathing ...");
+    }
+    protected void eating() {
+        System.out.println("the animal is eating ...");
+    }
+}
